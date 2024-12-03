a3babb1b4f6b15921e1ac5298b1c060149c53f5b
Kirill Kireev <karjaakirill@gmail.com>
2024-12-03 14:35:08 +0300

    первоначальный вывод и удаление

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/karisDev/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const placesItemTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".places__item");
 
 // @todo: DOM узлы
+const content = document.querySelector(".content");
+const placesList = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+const createCard = (item, onDelete) => {
+  const cardElement = placesItemTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+
+  cardElement.querySelector(".card__title").textContent = item.name;
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", onDelete);
+
+  return cardElement;
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (event) => event.target.parentNode.remove();
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => placesList.append(createCard(item, deleteCard)));
