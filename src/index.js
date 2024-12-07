4f413ecfe5613788de9f81532989497efe8ae2e8
Kirill Kireev <karjaakirill@gmail.com>
2024-12-07 13:30:50 +0300

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './pages/index.css';
+import {initialCards} from  "./components/cards";
+
 // @todo: Темплейт карточки
 const placesItemTemplate = document
   .querySelector("#card-template")
